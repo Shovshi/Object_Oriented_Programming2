a746aba952e4d266afd7e88ac6f83902a472daae
Shovshi <117938983+Shovshi@users.noreply.github.com>
2023-01-02 14:05:05 +0200

    Add files via upload

---
+++ Ex2_1.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.random.*;
+public class Ex2_1 {
+
+    static String path="C:\\\\Users\\\\User\\\\Desktop\\\\Eli\\"; //Check where we need to push the files
+    public static String[] createTextFiles(int n,int seed,int bound)
+    {
+        Random rand = new Random(seed);
+        String []names=new String[n];
+        boolean didSetLines=true;
+
+        for (int i = 0; i < n ; i++)
+        {
+            int lines= rand.nextInt(bound);
+            System.out.println("number : "+lines);
+            try
+            {
+                File file=new File(path+"file_" +(i+1)+".txt");
+
+                boolean didCreate=file.createNewFile();
+                if (didCreate)
+                {
+                    System.out.println("file "+(i+1)+ " created");
+                    names[i]="file_"+(i+1);
+                    didSetLines= setLines(file.getAbsolutePath(),lines);
+                    if(!didSetLines)
+                    {
+                        System.out.println("did not succeed to set lines");
+                        //return -1
+                    }
+                }
+                else
+                {
+                    System.out.println("file "+ (i+1)+ " did not created");
+                    return null;
+                }
+
+            }
+            catch(IOException e)
+            {
+                System.err.println("Error");
+            }
+
+
+
+        }
+
+        return names;
+
+    }
+
+    public static boolean setLines (String path, int lines)
+    {
+        try
+        {
+            FileWriter fw=new FileWriter(path);
+            PrintWriter outs=new PrintWriter(fw);
+            while(lines > 0)
+            {
+                outs.println("i love food");
+                lines--;
+            }
+            outs.close();
+            fw.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error");
+        }
+
+        return true;
+
+    }
+
+    public static int getNumOfLines(String[] fileNames)
+    {
+        int numOfLines=0;
+        int cnt=0;
+        for (int i = 0; i < fileNames.length ; i++)
+        {
+            cnt=0;
+            try
+            {
+                FileReader fr= new FileReader(path+fileNames[i]+".txt");
+                BufferedReader br= new BufferedReader(fr);
+                String line="";
+                while(line!=null)
+                {
+                    line= br.readLine();
+                    cnt++;
+                }
+                cnt--; //the last line br read is null
+                numOfLines+=cnt;
+                br.close();
+                fr.close();
+            }
+            catch (IOException e)
+            {
+                System.err.println("Error");
+            }
+        }
+        return numOfLines;
+    }
+
+    public int getNumOfLinesThreads(String[] fileNames)
+    {
+        int totalNumOfLines=0;
+        for (int i = 0; i <fileNames.length ; i++)
+        {
+            FileThread ft=new FileThread(fileNames[i]);
+            ft.run();
+            totalNumOfLines+= ft.getNum();
+        }
+        return totalNumOfLines;
+    }
+
+   public int getNumOfLinesThreadPool(String[] fileNames) throws ExecutionException, InterruptedException
+   {
+       int totalNumOfLines=0;
+       ExecutorService pool= Executors.newFixedThreadPool(fileNames.length);
+
+       for (int i = 0; i < fileNames.length ; i++)
+       {
+           FileCallable fc= new FileCallable(fileNames[i]);
+           Future<Integer> future =  pool.submit(fc);
+           totalNumOfLines += future.get();
+       }
+
+        pool.shutdown();
+       return totalNumOfLines;
+   }
+
+
+
+
+    public static String getPath()
+    {
+        return path;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        String names[]= Ex2_1.createTextFiles(3,10,100000);
+        int lines= Ex2_1.getNumOfLines(names);
+        System.out.println("total: "+lines);
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(names[i]);
+        }
+        Ex2_1 ex2 = new Ex2_1();
+        int x = ex2.getNumOfLinesThreads(names);
+        System.out.println("Thread: " +x);
+
+        int y = ex2.getNumOfLinesThreadPool(names);
+        System.out.println("Thread Pool: "+y);
+    }
+}
