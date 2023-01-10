1a4b558ebdef14d0595a70271939d9619e5e0da4
EliGreenblatt <eligreenblatt9@gmail.com>
2023-01-10 12:01:26 +0200

    Tidy Up Functions

--- PartB/CustomExecutor.java
+++ PartB/CustomExecutor.java
@@ -5,21 +5,14 @@ import java.util.concurrent.*;
 
 public class CustomExecutor<T> extends ThreadPoolExecutor
 {
-    int [] priorityArray = new int [11];
-    //Runtime runtime = Runtime.getRuntime();
-    //int minNumOfProcessors = runtime.availableProcessors()/2;
-    //int maxNumOfProcessors = runtime.availableProcessors()-1;
-    //PriorityBlockingQueue<Runnable> blockingQueue = new PriorityBlockingQueue<>(minNumOfProcessors,
-    //        (t1 , t2 ) -> ((Task) t1).compareTo((Task)t2));
-    //ThreadPoolExecutor pool = new ThreadPoolExecutor(minNumOfProcessors,maxNumOfProcessors,300,TimeUnit.MILLISECONDS,blockingQueue);
-
+    int [] priorityArray = new int [10];
     public CustomExecutor()
     {
         super(Runtime.getRuntime().availableProcessors() / 2, Runtime.getRuntime().availableProcessors() - 1,
                 300, TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>(Runtime.getRuntime().availableProcessors()/2,
-                        Comparator.comparing(t -> ((TaskAdapt) t))));
+                        Comparator.comparing(t -> ((TaskAdapt<T>) t))));
     }
-    public Future<T> submit (Task task)
+    public Future<T> submit (Task<T> task)
     {
         priorityArray[task.taskType.getPriorityValue()]++;
         return super.submit((Callable<T>) task);
@@ -40,7 +33,7 @@ public class CustomExecutor<T> extends ThreadPoolExecutor
     public int getCurrentMax()
     {
         int maxPriority = 0;
-        for (int i = 10; i >= 0; i--)
+        for (int i = 9; i >= 0; i--)
         {
             if (priorityArray[i] > 0)
             {
@@ -48,7 +41,7 @@ public class CustomExecutor<T> extends ThreadPoolExecutor
             }
         }
         System.out.println("The Queue is empty");
-        return 0;
+        return -1;
     }
 
     public void gracefullyTerminated()
@@ -66,5 +59,4 @@ public class CustomExecutor<T> extends ThreadPoolExecutor
     {
         return new TaskAdapt<T>(callable);
     }
-
 }
--- PartB/TaskAdapt.java
+++ PartB/TaskAdapt.java
@@ -3,37 +3,43 @@ package PartB;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
-    public class TaskAdapt<T> extends FutureTask<T>  implements Comparable<TaskAdapt>
+    public class TaskAdapt<T> extends FutureTask<T>  implements Comparable<TaskAdapt<T>>
     {
 
-        private Callable<T> callable;
+        private final Task<T> task;
 
         public TaskAdapt(Callable<T> callable)
         {
             super(callable);
-            this.callable = callable;
+            this.task = (Task<T>)callable;
+        }
+
+        public TaskAdapt(Task task)
+        {
+            super(task);
+            this.task = task;
         }
 
         public Callable<T> getCallable()
         {
-            return this.callable;
+            return this.task.function;
         }
 
         public int getPriority()
         {
-            return (((Task<T>)callable)).taskType.getPriorityValue();
+            return ((task).taskType.getPriorityValue());
         }
         @Override
         public int compareTo(TaskAdapt o)
         {
 
-            if (((Task<T>)(this.callable)).taskType.getPriorityValue() >
-                    ((Task<T>)(o.getCallable())).taskType.getPriorityValue())
+            if (task.taskType.getPriorityValue() >
+                    task.taskType.getPriorityValue())
             {
                 return 1;
             }
-            if (((Task<T>)(this.callable)).taskType.getPriorityValue() ==
-                    ((Task<T>)(o.getCallable())).taskType.getPriorityValue())
+            if (task.taskType.getPriorityValue() ==
+                    task.taskType.getPriorityValue())
             {
                 return 0;
             }
